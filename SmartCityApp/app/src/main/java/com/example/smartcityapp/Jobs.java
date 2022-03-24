a9b7cd015e0ce464d5ea43e7ffae59a22190f907
Arjun Kavungal <arjunkavungal@gmail.com>
2022-03-23 21:05:03 -0400

    Make the job api

--- SmartCityApp/app/src/main/java/com/example/smartcityapp/Jobs.java
+++ SmartCityApp/app/src/main/java/com/example/smartcityapp/Jobs.java
@@ -1,5 +1,6 @@
 package com.example.smartcityapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,47 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.careerjet.webservice.api.Client;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpUpgradeHandler;
+import javax.servlet.http.Part;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,11 +65,14 @@ public class Jobs extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private Context mContext;
     public Jobs() {
         // Required empty public constructor
     }
-
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext=context;
+    }
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -59,6 +104,38 @@ public class Jobs extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jobs, container, false);
+        View v = inflater.inflate(R.layout.fragment_jobs, container, false);
+        TextView t = (TextView) v.findViewById(R.id.t);
+        Button button = (Button) v.findViewById(R.id.jobsButton);
+        EditText e = (EditText) v.findViewById(R.id.editTextTextPersonName);
+        RequestQueue queue = Volley.newRequestQueue(mContext);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET,  "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=f46c8c2b&app_key=8272536efc4c26fc6491fb097baa697b&results_per_page=20&&what=" + e.getText().toString() + "&where=30605&sort_by=salary&salary_min=30000&full_time=1&permanent=1&content-type=application/json", null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray results = response.getJSONArray("results");
+                            t.setText("");
+                            for (int i = 0; i < results.length(); i++) {
+
+                                t.append(results.getJSONObject(i).getString("title").toString());
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                queue.add(jsonObjectRequest);
+            }
+        });
+        return v;
     }
 }
